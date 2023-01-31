47d5f33af964bfefcff3fea0d5b62f6325a1c355
VegarusNik <mv-nikolay@vegarus.su>
2023-01-31 16:47:00 +0300

    FIx and test flake8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
+
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
